72f9ac465f6f0147e30f89c633e850b59eb79a04
Bilal <bmalik@gmail.com>
2024-12-24 13:21:47 -0500

    Dashboard Screen

---
+++ Sources/BamwareUI/Screens/DashboardScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  BamwareUI
+//
+//  Created by Bilal Malik on 12/24/24.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct DashboardScreen: View {
+    var body: some View {
+        Text("Dashboard Screen")
+    }
+}
+
+@available(iOS 13.0, *)
+#Preview {
+    DashboardScreen()
+}
